b2031c1252aa3a461daa0d9b3b369798e1c8fe3b
vipin-gurjar <vpdogne.12345@gmail.com>
2023-12-20 16:14:57 +0530

    eight

--- iqnaax/settings.py
+++ iqnaax/settings.py
@@ -111,21 +111,30 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, 'home/static')
-]
+#     os.path.join(BASE_DIR, 'home/static')
+# ]
 
 
-if not DEBUG:
+# if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+    
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"), "templates"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
