78957d6bf2fa0006178f82b5aacd5571db69d462
Vipin Dogne <138565328+vipin-gurjar@users.noreply.github.com>
2023-12-26 15:55:02 +0530

    Update models.py

--- about/models.py
+++ about/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 # Create your models here.    
 class We_Different(models.Model):
     title = models.CharField(max_length=200)
-    different_Img = models.ImageField(upload_to='Different_Images')
+    different_Img = CloudinaryField('Different_Images')
     
     def __str__(self):
         return str(self.id)
@@ -23,7 +23,7 @@ class About_image_gallery(models.Model):
 class Reason_to_shop(models.Model):
     title = models.CharField(max_length=200)
     desc = models.TextField()
-    medalImg = models.ImageField(upload_to='Medal Images')
+    medalImg = CloudinaryField('Medal Images')
     
     def __str__(self):
         return str(self.id)
